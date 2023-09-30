071c0927d9d3d11c0991b2c1eab7717486c23497
Liron Ostrovsky <liron.ostrovsky@gmail.com>
2023-09-30 19:50:34 +0300

    Fixed bug in home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,57 +6,18 @@ import MovieCard from './MovieCard';
 import { Stack, Typography, Paper } from '@mui/material'
 import Container from '@mui/material/Container';
 import notify from '../utils/ErrorToast';
+import Movie from '../interfaces/Movie/Movie';
+import { GenreKeys, genresMap } from '../interfaces/Genre';
 
 interface MovieData {
     movies: Movie[];
     loading: boolean;
-    // loadMoreCounter: number;
-    // noMoreVideos: boolean;
 }
 
-interface Movie {
-    id: number;
-    title: string;
-    description: string;
-    duration: number;
-    releaseDate: string;
-    genre: string;
-    director: string;
-    language: string;
-    minAge: number;
-    createdOn: string;
-}
-
-const Genres: any = {
-    ACTION: {
-        label: 'אקשן',
-    },
-    COMEDY: {
-        label: 'קומדיה',
-    },
-    HORROR: {
-        label: 'אימה',
-    },
-    DRAMA: {
-        label: 'דרמה',
-    },
-    KIDS: {
-        label: 'עולם הילדים',
-    },
-};
+const genresKeysArr: GenreKeys[] = Array.from(genresMap.keys());
 
 const Home: React.FC = () => {
-    const initialState = Object.keys(Genres).reduce((acc: any, key: string) => {
-        acc[key] = {
-            movies: [],
-            loading: true,
-        };
-        return acc;
-    }, {});
-
-    const [moviesByGenre, setMoviesByGenre] = useState<Record<string, MovieData>>(initialState);
-    const [movieOrder, setMovieOrder] = useState(Object.keys(Genres));
-
+    const [moviesByGenre, setMoviesByGenre] = useState<Map<GenreKeys, Movie[]>>(new Map());
 
     const { token } = useAuth();
 
@@ -64,9 +25,11 @@ const Home: React.FC = () => {
         fetchMovies();
     }, []);
 
-    const fetchMovies = () => {
-        Object.keys(Genres).forEach(async genre => {
-            try {
+    const fetchMovies = async () => {
+        const updatedMap: Map<GenreKeys, Movie[]> = new Map(moviesByGenre);
+
+        try {
+            await Promise.all(genresKeysArr.map(async (genre: GenreKeys) => {
                 const response = await fetch(`http://localhost:8080/api/movie/genre/${genre}`, {
                     headers: {
                         'Authorization': `Bearer ${token}`
@@ -77,46 +40,35 @@ const Home: React.FC = () => {
                 const data = await response.json();
 
                 if (response.ok) {
-                    setMoviesByGenre(() => ({
-                        [genre]: {
-                            movies: data,
-                            loading: false,
-                        }
-                    }));
+                    updatedMap.set(genre, data);
                 } else {
                     throw new Error(data.errors[0]);
                 }
-            } catch (error: any) {
-                notify(error.message);
-            }
-        });
-    };
+            }));
 
+            setMoviesByGenre(updatedMap);
+        } catch (error: any) {
+            notify(error.message);
+        }
+    };
 
     return (
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
             <Toolbar />
-
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }} >
+            <Box  >
                 {
-                    movieOrder.map((genre: string) => (
+                    Array.from(moviesByGenre.entries()).map(([genre, movies]) => (
                         <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1.5vh' }}>
-
-                            <Typography variant='h6' fontWeight={'bold'}>{Genres[genre].label}</Typography >
+                            <Typography variant='h6' fontWeight={'bold'}>{genresMap.get(genre)}</Typography >
                             <Box sx={{ display: 'flex', gap: '3vw' }}>
-                                {moviesByGenre[genre]?.movies.map((movie: Movie) => (
+                                {movies.map((movie: Movie) => (
                                     <MovieCard title={movie.title} thumnailURL={`http://localhost:8080/api/movie/thumbnail/${movie.id}`} />
                                 ))}
                             </Box>
-
                         </Box>
-
-
                     ))
-
                 }
             </Box>
-
         </Box>
     );
 };
